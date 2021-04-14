3d66d55573c9a6d01b5cf99fbec63e830e809151
sheycc <shsljm2903@gmail.com>
2021-04-14 15:49:22 -0400

    Add some arithmetic and logical operation nodes

--- src/parsing/operations_nodes.py
+++ src/parsing/operations_nodes.py
@@ -14,3 +14,44 @@ class NotNode(ExpressionNode):
     def __init__(self, expression):
         super(NotNode, self).__init__()
         self.expression = expression
+
+class LogicNotNode(ExpressionNode):
+    def __init__(self, expression):
+        super(LogicNotNode, self).__init__()
+        self.expression = expression
+
+class SumNode(ArithBinOpNode):
+    def __init__(self, left, right):
+        super(SumNode, self).__init__()
+        self.left = left
+        self.right = right
+
+class SubNode(ArithBinOpNode):
+    def __init__(self, left, right):
+        super(SubNode, self).__init__()
+        self.left = left
+        self.right = right
+
+class MultNode(ArithBinOpNode):
+    def __init__(self, left, right):
+        super(MultNode, self).__init__()
+        self.left = left
+        self.right = right
+
+class DivNode(ArithBinOpNode):
+    def __init__(self, left, right):
+        super(DivNode, self).__init__()
+        self.left = left
+        self.right = right
+
+class LessNode(LogicBinOpNode):
+    def __init__(self, left, right):
+        super(LessThanNode, self).__init__()
+        self.left = left
+        self.right = right
+
+class LessEqualNode(LogicBinOpNode):
+    def __init__(self, left, right):
+        super(LessOrEqualThanNode, self).__init__()
+        self.left = left
+        self.right = right
