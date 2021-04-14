1b28cf7dca07a3c677396149a9108aa9f4471067
sheycc <shsljm2903@gmail.com>
2021-04-14 12:36:15 -0400

    Add more expression nodes

--- src/parsing/expr_nodes.py
+++ src/parsing/expr_nodes.py
@@ -19,3 +19,49 @@ class StaticCallNode(ExpressionNode):
         self.static_type = static_type
         self.method = method
         self.args = args
+
+class AssignNode(ExpressionNode):
+    def __init__(self, name, expression):
+        super(AssignNode, self).__init__()
+        self.name = name
+        self.expression = expression
+
+class IfNode(ExpressionNode):
+    def __init__(self, predicate, then_expr, else_expr):
+        super(IfNode, self).__init__()
+        self.predicate = predicate
+        self.then_expr = then_expr
+        self.else_expr = else_expr
+
+class WhileNode(ExpressionNode):
+    def __init__(self, predicate, expression):
+        super(WhileNode, self).__init__()
+        self.predicate = predicate
+        self.expression = expression
+
+class BlockNode(ExpressionNode):
+    def __init__(self, expr_list):
+        super(BlockNode, self).__init__()
+        self.expr_list = expr_list
+
+class LetNode(ExpressionNode):
+    def __init__(self, init_list, body):
+        super(LetNode, self).__init__()
+        self.init_list = init_list
+        self.body = body
+
+class CaseNode(ExpressionNode):
+    def __init__(self, expression, act_list):
+        super(CaseNode, self).__init__()
+        self.expression = expression
+        self.act_list = act_list
+
+class NewNode(ExpressionNode):
+    def __init__(self, new_type):
+        super(NewNode, self).__init__()
+        self.new_type = new_type
+
+class IsVoidNode(ExpressionNode):
+    def __init__(self, expression):
+        super(IsVoidNode, self).__init__()
+        self.expression = expression
