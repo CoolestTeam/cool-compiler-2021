4797fa0a3e9ef5e56ed88272ef3b8a75ff0f42ef
sheycc <shsljm2903@gmail.com>
2021-04-15 15:06:53 -0400

    Add some basic tokens for the tokens list

--- src/parsing/lexer.py
+++ src/parsing/lexer.py
@@ -5,9 +5,40 @@ class MyLexer():
     def __init__(self):
         self.build()
 
+    # The tokens list
+    def get_basic_tok(self):
+        return [
+            "INTEGER",
+            "STRING",
+            "BOOLEAN",
+            "PLUS",
+            "MINUS",
+            "MULTIPLY",
+            "DIVIDE",
+            "EQUAL",
+            "LESS",
+            "LESSEQ",
+            "ASSIGN",
+            "NOX",
+            "ID",
+            "TYPE",
+            "LPAREN",
+            "RPAREN",
+            "LBRACE",
+            "RBRACE",
+            "COLON",
+            "SEMIC",
+            "COMMA",
+            "DOT",
+            "ARROBA",
+            "ARROW"
+        ]
+
     # Build the lexer
     def build(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
+
+
 
 if __name__ == "__main__":
     _file = sys.argv[1]
